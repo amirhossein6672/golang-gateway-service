2890ffbdc7ddd3b7bffeb057d7bc72a6980b92fb
amirhossein noorollahzadegan <amirhossein.nr2018@gmail.com>
2024-12-15 15:51:48 +0300

    _

--- main.go
+++ main.go
@@ -81,9 +81,9 @@ func main() {
 	c := cors.New(cors.Options{
 		// AllowedOrigins defines which origins are permitted to access the resources.
 		// "*" allows all origins; in production, specify the exact origins.
-		AllowedOrigins: []string{"*"},
-		AllowedHeaders: []string{"*"},
-		AllowedMethods: []string{"*"},
+		// AllowedOrigins: []string{"*"},
+		// AllowedHeaders: []string{"*"},
+		// AllowedMethods: []string{"*"},
 		// AllowCredentials indicates whether the request can include user credentials like cookies.
 		AllowCredentials: true,
 	})
